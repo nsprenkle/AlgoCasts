10036926a46c39f84d9eba7c0c54eb04f2087b66
Nathan Sprenkle <nathan.sprenkle@gmail.com>
2019-05-13 23:07:29 -0400

    Add vowel search solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+/* RegExp solution */
+function vowels (str) {
+  let matches = str.match(/[aeiou]/gi)
 
-module.exports = vowels;
+  return matches ? matches.length : 0
+}
+
+/* My iterative approach */
+// function vowels (str) {
+//   let vowelCount = 0
+//   const vowelList = ['a', 'e', 'i', 'o', 'u']
+
+//   for (let letter of str.toLowerCase()) {
+//     if (vowelList.includes(letter)) {
+//       vowelCount++
+//     }
+//   }
+
+//   return vowelCount
+// }
+
+module.exports = vowels
