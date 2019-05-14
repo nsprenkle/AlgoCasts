3ec7fb26e1dbafbcd5c790c5f02cba885d115a84
Nathan Sprenkle <nathan.sprenkle@gmail.com>
2019-05-13 23:11:32 -0400

    Add link list solution

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,74 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor (data, node) {
+    this.data = data
+    this.next = node || null
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor () {
+    this.head = null
+  }
 
-module.exports = { Node, LinkedList };
+  insertFirst (data) {
+    let second = this.head
+    this.head = new Node(data, second)
+  }
+
+  size () {
+    let size = 0
+    let node = this.head
+    debugger
+
+    while (node) {
+      size += 1
+      node = node.next
+    }
+
+    return size
+  }
+
+  getFirst () {
+    return this.head
+  }
+
+  getLast () {
+    let node = this.head
+
+    while (node && node.next) {
+      node = node.next
+    }
+
+    return node
+  }
+
+  clear () {
+    this.head = null
+  }
+
+  removeFirst () {
+    this.head = this.head.next
+  }
+
+  removeLast () {
+    let node = this.head
+    let prev
+
+    while (node) {
+      prev = node
+      node = node.next
+    }
+
+    prev.next = null
+  }
+}
+
+let ll = new LinkedList()
+ll.insertFirst(1)
+ll.insertFirst(2)
+console.log(ll.size())
+
+module.exports = { Node, LinkedList }
