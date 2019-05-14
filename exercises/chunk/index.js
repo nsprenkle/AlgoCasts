31063632130e6edb63ff46e210930d75554ccf06
Nathan Sprenkle <nathan.sprenkle@gmail.com>
2019-05-13 23:06:00 -0400

    Add array chunking solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,47 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk (array, size) {
+  let chunked = []
+  let index = 0
 
-module.exports = chunk;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size))
+    index += size
+  }
+
+  return chunked
+}
+
+// function chunk (array, size) {
+//   let chunked = []
+
+//   for (let i = 0; i < array.length; i += size) {
+//     let begin = i
+//     let end = begin + size
+
+//     chunked.push(array.slice(begin, end))
+//   }
+
+//   return chunked
+// }
+
+// function chunk (array, size) {
+//   const chunked = []
+//
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1]
+//
+//     if (!last || last.length === size) {
+//       // add new chunk if chunk doesn't exist or is full
+//       chunked.push([element])
+//     } else {
+//       // otherwise, add element to the current chunked
+//       last.push(element)
+//     }
+//   }
+//
+//   return chunked
+// }
+
+module.exports = chunk
