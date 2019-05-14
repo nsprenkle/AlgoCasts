8dd1fee87505d8849a96614ceffa1d0b8c0c0267
Nathan Sprenkle <nathan.sprenkle@gmail.com>
2019-05-13 23:04:47 -0400

    Add string reversal solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse (str) {
+  return str.split('').reduce((reversed, character) =>
+    character + reversed
+  , '')
+}
 
-module.exports = reverse;
+// function reverse (str) {
+//   var reversed = ''
+//
+//   for (let character of str) {
+//     reversed = character + reversed
+//   }
+//
+//   return reversed
+// }
+
+// function reverse (str) {
+//   return str.split('').reverse().join('')
+// }
+
+module.exports = reverse
