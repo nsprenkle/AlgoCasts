d901207d678091a74e8510dee4e6b4da01e15699
Nathan Sprenkle <nathan.sprenkle@gmail.com>
2019-05-13 23:06:36 -0400

    Add sentence capitalization solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,31 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize (str) {
+  let capitalized = []
 
-module.exports = capitalize;
+  for (let word of str.split(' ')) {
+    capitalized.push(word[0].toUpperCase() + word.slice(1))
+  }
+
+  return capitalized.join(' ')
+}
+
+// function capitalize (str) {
+//   let result = str[0].toUpperCase()
+
+//   for (let i = 1; i < str.length; i++) {
+//     let previous = str[i - 1] || ' '
+//     let current = str[i]
+
+//     if (previous === ' ') {
+//       result += current.toUpperCase()
+//     } else {
+//       result += current
+//     }
+//   }
+
+//   return result
+// }
+
+module.exports = capitalize
