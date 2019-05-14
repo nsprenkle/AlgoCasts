ad1a310ae6de8e5c167e204ad36862b79ec8ca8b
Nathan Sprenkle <nathan.sprenkle@gmail.com>
2019-05-13 23:05:34 -0400

    Add MaxChars solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar (str) {
+  let charMap = {}
 
-module.exports = maxChar;
+  for (let char of str) {
+    charMap[char] = charMap[char]++ || 1
+  }
+
+  let max = { count: 0, char: '' }
+
+  for (let char in charMap) {
+    if (charMap[char] > max.count) {
+      max.count = charMap[char]
+      max.char = char
+    }
+  }
+
+  return max.char
+}
+
+module.exports = maxChar
