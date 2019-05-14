ec4389c16cf2047736dc174cab45b086077b4ae3
Nathan Sprenkle <nathan.sprenkle@gmail.com>
2019-05-13 23:06:59 -0400

    Add print steps solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,54 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+/* My original solution */
+function steps (n) {
+  for (let i = 1; i <= n; i++) {
+    console.log('#'.repeat(i) + ' '.repeat(n - i))
+  }
+}
 
-module.exports = steps;
+/* Their recursive solution */
+// function steps (n, row = 0, stair = '') {
+//   if (n === row) { return }
+
+//   if (n === stair.length) {
+//     console.log(stair)
+//     steps(n, row + 1)
+//     return
+//   }
+
+//   stair += stair.length <= row ? '#' : ' '
+//   steps(n, row, stair)
+// }
+
+/* My recursive solution */
+// function steps (n) {
+//   function step (row, n) {
+//     if (row > n) { return }
+
+//     console.log('#'.repeat(row) + ' '.repeat(n - row))
+//     step(row + 1, n)
+//   }
+
+//   step(1, n)
+// }
+
+/* Iterative solution */
+// function steps (n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = ''
+
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#'
+//       } else {
+//         stair += ' '
+//       }
+//     }
+
+//     console.log(stair)
+//   }
+// }
+
+module.exports = steps
